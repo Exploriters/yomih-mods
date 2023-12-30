fcdab63f27ae75fe319ef0cd8e8166a856f2600b
Abrel Beta <3010275592@qq.com>
2023-12-30 20:14:05 +0800

    小蓝：【开门】你好！有我的信件……啊……【看着中文】啊？

--- !Just-someModdingCharacters/Characters/sixgarss/states/fight back.gd
+++ !Just-someModdingCharacters/Characters/sixgarss/states/fight back.gd
@@ -1,14 +1,13 @@
 extends CharacterState
 
-
-
 func _tick():
 	host.apply_forces()
 	
+func _frame_2():
+	host.apply_force(-8 * host.get_facing_int(), 0)
 	
-	
-func _frame_3():
-	host.apply_force(-4, 0)
-	
-func _frame_8():
-	host.apply_force(15, 0)
+func _frame_11():
+	host.apply_force(25 * host.get_facing_int(), 0)
+
+func _frame_15():
+	host.apply_force(-10 * host.get_facing_int(), 0)
