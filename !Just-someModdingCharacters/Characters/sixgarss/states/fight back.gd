1695999885499929dd0d012288269541a54176de
sixgrass <1449703449@qq.com>
2024-01-01 22:18:04 +0800

    神明降下了恩赐 后撤步获得了无敌的力量 但是力量过于强大身躯难以承载 使用会导致世界崩塌 我还需要再进行熟练这种力量 在那之前 再也不会使用了

--- !Just-someModdingCharacters/Characters/sixgarss/states/fight back.gd
+++ !Just-someModdingCharacters/Characters/sixgarss/states/fight back.gd
@@ -3,11 +3,19 @@ extends CharacterState
 func _tick():
 	host.apply_forces()
 	
-func _frame_2():
-	host.apply_force(-8 * host.get_facing_int(), 0)
 	
+
+	
+func _frame_2():
+	host.start_invulnerability()
+	host.apply_force(-8 * host.get_facing_int(), 0)
+	#你感觉到有奇怪的力量在此徘徊，导致这个无敌帧会引起崩溃
+
 func _frame_11():
-	host.apply_force(25 * host.get_facing_int(), 0)
+	host.end_invulnerablity()
+	#奇怪的力量范围及其之广，这里也会崩溃
+	host.apply_force(30 * host.get_facing_int(), 0)
+
 
 func _frame_15():
-	host.apply_force(-10 * host.get_facing_int(), 0)
+	host.apply_force(-5 * host.get_facing_int(), 0)
